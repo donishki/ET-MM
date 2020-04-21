bd30f108b56dd0622452ad431640e2e371c49230
keith miller <keith.miller@thermosystems.com>
2020-04-21 16:19:29 -0400

    Yeah this is why we should test first... Done for today.

--- setup.sh
+++ setup.sh
@@ -6,10 +6,13 @@ if [ $EUID -ne 0 ]; then
     exit 1
 fi
 
+# set environment variables
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # prepare setup scripts for execution
 printf "Preparing setup scripts..."
 chmod +x $BASEDIR/setup/environment.sh
-chmod +x $BASEDOR/setup/database.sh
+chmod +x $BASEDIR/setup/database.sh
 printf "\t[OK]\n"
 
 # execute environment setup script
