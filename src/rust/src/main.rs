3d3ddbfe60ff7f7a5df3bafe43db43e5e91cc098
keith miller <keith.miller@thermosystems.com>
2020-05-19 13:14:08 -0400

    start cleaning up code now

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -9,6 +9,9 @@ mod database;
 #[macro_use]
 mod logger;
 
+use bot::Bot;
+use config::Config;
+use database::Database;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
@@ -16,19 +19,16 @@ use tokio::sync::RwLock;
 async fn main() {
     // initialize logger
     let log_lock = Arc::new(RwLock::new(logger::Log::new()));
+    let log = log_lock.read().await;
     // FIXME: eventually this will be where arguements are processed
     //        for now just hardcode these parameters
     // initialize bot
-    // {
-        let log = log_lock.read().await;
-        info!(log.logger, "ET-MM Bot version {}", env!("CARGO_PKG_VERSION"));
-    // }
+    info!(log.logger, "ET-MM Bot version {}", env!("CARGO_PKG_VERSION"));
     // load bot configuration
     let bot_config_path: &'static str = "/opt/et-mm-bot/config.cfg";
     let config = {
-        // let log = log_lock.read().await;
         info!(log.logger, "loading configuration into memory...");
-        match config::Config::construct(bot_config_path) {
+        match Config::construct(bot_config_path) {
             Ok (b) => b,
             Err(e) => {
                 error!(log.logger, "\t{}", e; "file" => bot_config_path);
@@ -40,9 +40,8 @@ async fn main() {
     };
     // initialize database object
     let database_lock = {
-        // let log = log_lock.read().await;
         info!(log.logger, "initializing database object...");
-        match database::Database::construct(&config, &log_lock).await {
+        match Database::construct(&config, &log_lock).await {
             Ok (d) => Arc::new(RwLock::new(d)),
             Err(e) => {
                 error!(log.logger, "\t{}", e; "connection string" => config.database_connection_string);
@@ -54,9 +53,8 @@ async fn main() {
     };
     // add match making groups to database
     {
-        // let log = log_lock.read().await;
-        let database = database_lock.read().await;
         info!(log.logger, "adding configured match making groups...");
+        let database = database_lock.read().await;
         match database.add_mm_groups(&config).await {
             Ok (_) => (),
             Err(e) => {
@@ -69,14 +67,10 @@ async fn main() {
     }
     // initialize discord bot
     let mut bot = {
-        {
-            // let log = log_lock.read().await;
-            info!(log.logger, "initializing discord bot...");
-        }
-        match bot::Bot::construct(&config, &database_lock, &log_lock).await {
+        info!(log.logger, "initializing discord bot...");
+        match Bot::construct(&config, &database_lock, &log_lock).await {
             Ok (b) => b,
             Err(e) => {
-                // let log = log_lock.read().await;
                 error!(log.logger, "\t{}", e);
                 drop(log);
                 drop(log_lock);
@@ -85,13 +79,10 @@ async fn main() {
         }
     };
     // start bot
-    {
-        // let log = log_lock.read().await;
-        info!(log.logger, "starting discord bot...");
-        if let Err(e) = bot.start().await {
-            error!(log.logger, "\t{}", e);
-            drop(log);
-            panic!();
-        };
-    }
+    info!(log.logger, "starting discord bot...");
+    if let Err(e) = bot.start().await {
+        error!(log.logger, "\t{}", e);
+        drop(log);
+        panic!();
+    };
 }
